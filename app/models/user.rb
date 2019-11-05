940cd2630ad0b5be8a7e5691052f9e669b3d5fce
leovanel <leovanel>
2019-11-05 09:53:01 +0100

    commit2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+
+  after_create :welcome_send
   
   has_many :attendances 
   has_many :events , through: :attendances 
   has_many :events
   
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
