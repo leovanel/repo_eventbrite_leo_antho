11927b30c304e7df8e66920988e3247fe66e2afe
leovanel <leovanel>
2019-11-05 18:20:45 +0100

    commit de la mort

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,15 +4,36 @@ class EventsController < ApplicationController
   end
   
   def show
-    
+    @detail = Event.find(params[:id])
+    @events = Event.showall
   end
 
   def new
-    
+    @detail = Event.new
+    user=current_user
   end
 
   def create
+    user=current_user
     
+    @detail = Event.new( user_id: user.id,
+                          'title' => params[:title],
+                          'description' => params[:description],
+                          'start_date' => params[:start_date],
+                          'duration' => params[:duration],
+                          'price' => params[:price],
+                          'location' => params[:location]
+                        ) # avec xxx qui sont les données obtenues à partir du formulaire
+    
+      if @detail.save # essaie de sauvegarder en base @gossip
+      @events = Event.showall
+      
+      redirect_to event_path(@detail.id) # si ça marche, il redirige vers la page d'index du site
+    else
+      render new_event_path  # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+    end
   end
+    
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_current_user, only: [:show]
   def show
     @profil = User.find(params['id'])
+    @events = Event.showall
   end
+
+private
+
+def authenticate_current_user
+  @profil = User.find(params['id'])
+  unless current_user.id == @profil.id
+    flash[:danger] = "Vous n'avez pas acces a cette page"
+   redirect_to '/'
+  end
+end
+
 end
